62a366e25e3dca313a4e5c8c51f0a18224cd6a3c
saiganesh <ganeshsaiganesh434@gmail.com>
2026-08-29 22:56:28 +0530

    mini2

---
+++ mini2/addition.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace  std;
+class Addition{
+   public:
+    static int add( int a,int b){
+    cout<<"sum :"<<a+b<<endl;
+
+
+   }
+
+};
+int main(){
+    Addition obj;
+    obj.add(4,5);
+
+
+}
+
+    
+
